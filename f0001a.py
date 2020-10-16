6015f3091c07f09a70cd94df1cde779455164f38
violetta2413 <71691747+violetta2413@users.noreply.github.com>
2020-10-16 08:43:19 +0200

    Add files via upload

---
+++ f0001a.py
@@ -0,0 +1,5 @@
+#F0001a: Mi történik, ha lefuttatjuk ezt a programot? A program futtatása nélkül fogalmazd meg magadnak – aztán persze lefuttathatod, hogy lásd, igazad volt-e:)
+
+print("...Hogyha én ezt tudtam volna")
+print("Köpönyeget hoztam vóna")
+
